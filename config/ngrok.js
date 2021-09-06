8f920507d202c4d5b96b39358d8e30ac34697a4e
murilo <murilo.laface@cactvs.com.br>
2021-09-06 09:06:04 -0300

    fix possible error on start ngrok

--- config/ngrok.js
+++ config/ngrok.js
@@ -1,16 +1,18 @@
 const ngrok = require("ngrok");
 const pipedrive_webhook = require("./webhook");
 const pw = new pipedrive_webhook();
-require('dotenv/config');
+require("dotenv/config");
 
 (async function () {
   //exposing my localhost to web using ngrok
-  const url = await ngrok.connect(process.env.PORT);
-  console.log(url)
-  const response = await pw.create_webhook(url + "/async/opportunity");
-  if (response.status == "ok") {
-    console.log("CREATED AND CONFIGURATED A WEBHOOK");
-  } else {
-    console.log("ERROR CREATED AND CONFIGURATED A WEBHOOK");
-  }
+  ngrok.connect(process.env.PORT).then((url) => {
+    console.log(url);
+    pw.create_webhook(url + "/async/opportunity").then((response) => {
+      if (response.status == "ok") {
+        console.log("CREATED AND CONFIGURATED A WEBHOOK");
+      } else {
+        console.log("ERROR CREATED AND CONFIGURATED A WEBHOOK");
+      }
+    });
+  });
 })();
